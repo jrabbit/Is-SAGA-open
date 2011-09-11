934c05aa8526db7a90e7ceaff5cc427db1c44835
Jrabbit <jackjrabbit+github@gmail.com>
2011-09-11 19:14:06 -0400

    basic app?

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [compojure "0.6.4"]
-                 [date-clj "1.0.1"]])
+                 [date-clj "1.0.1"]]
+  :dev-dependencies [[lein-ring "0.4.5"]]
+  :ring {:handler is_saga_open.core/app})
--- src/is_saga_open/core.clj
+++ src/is_saga_open/core.clj
@@ -13,4 +13,14 @@
 (defn night-truck []
   (if (is? (today) :monday) false (between? (today) (set-date (today) :hour 23 :minute 0) (set-date (tomorrow) :hour 3 :minute 0) )
   ))
+
 (if (is? (today) :weekend) (weekends) (weekday))
+
+
+(defroutes main-routes
+  (GET "/" [] "<h1>Hello World Wide Web!</h1>")
+  (route/resources "/")
+  (route/not-found "Page not found"))
+
+(def app
+  (handler/site main-routes))
