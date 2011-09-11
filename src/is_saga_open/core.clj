04ad55ba42219a5b5b6dd07ba0aa6eeddb661b24
Jrabbit <jackjrabbit+github@gmail.com>
2011-09-11 19:41:26 -0400

    increase readability

--- src/is_saga_open/core.clj
+++ src/is_saga_open/core.clj
@@ -2,19 +2,20 @@
   (:use compojure.core date-clj [hiccup core page-helpers])
   (:require [compojure.route :as route]
             [compojure.handler :as handler]))
+(defn open [h1 m1 h2 m2]
+  (between? (today) 
+            (set-date (today) :hour h1 :minute m1 :second 0) 
+            (set-date (today) :hour h2 :minute m2 :second 0))
+  )
 (defn weekends []
-  (or (between? (today) (set-date (today) :hour 17 :minute 0) (set-date (today) :hour 19 :minute 0))
-      (between? (today) (set-date (today) :hour 10 :minute 30) (set-date (today) :hour 13 :minute 0))
+  (or (open 17 0 19 0) (open 10 30 13 0) 
   ))
 (defn weekday []
-  (or (between? (today) (set-date (today) :hour 7 :minute 30) (set-date (today) :hour 16 :minute 0))
-      (between? (today) (set-date (today) :hour 17 :minute 0) (set-date (today) :hour 19 :minute 0))
+  (or (open 7 30 16 0) (open 17 0 19 0)
     ))
 (defn night-truck []
   (if (is? (today) :monday) false (between? (today) (set-date (today) :hour 23 :minute 0) (set-date (tomorrow) :hour 3 :minute 0) )
   ))
-
-
 
 (defn index-pg []
   (html5 [:head [:title "Is SAGA Open?"] (include-css "/css/style.css")]
